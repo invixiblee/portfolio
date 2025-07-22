eff0af8bc0a1bc875fad3efaf2b0a05c6acba355
Tazir <tazirxxx@gmail.com>
2025-07-22 15:25:07 +0300

    Add app description, image, and links

--- main.py
+++ main.py
@@ -23,13 +23,19 @@ content2 = "Below you can find some of the projects I have worked on. Feel free 
 st.write(content2)
 
 
-col3, col4 = st.columns(2)
+col3, colempty, col4 = st.columns([1.7, 0.1, 1.7])
 df = pandas.read_csv('data.csv', sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['url']})")
