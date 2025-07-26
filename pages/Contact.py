a5c7979a1b5b10b0cdc3cd39c53df98fd4cab4e9
Tazir <tazirxxx@gmail.com>
2025-07-26 12:58:43 +0300

    Created form without email sending

--- pages/Contact.py
+++ pages/Contact.py
@@ -2,3 +2,10 @@
 import streamlit as st
 
 st.title('Contact Me!')
+
+with st.form(key='form'):
+    useremail = st.text_input("Your Email Adress:")
+    message = st.text_area("Your Message:")
+    button = st.form_submit_button('Submit')
+    if button:
+        message = message + useremail
