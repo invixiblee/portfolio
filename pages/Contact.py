8b663375d67f3c3fb27ea27c351b873089392373
Tazir <tazirxxx@gmail.com>
2025-07-27 13:49:08 +0300

    Send email through function and form

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,11 +1,19 @@
 
 import streamlit as st
+from send_email import semail
 
 st.title('Contact Me!')
 
 with st.form(key='form'):
     useremail = st.text_input("Your Email Adress:")
-    message = st.text_area("Your Message:")
+    rawmessage = st.text_area("Your Message:")
+    message = f"""\
+Subject: Portfolio Contact - From {useremail}
+
+From: {useremail}
+{rawmessage}
+"""
     button = st.form_submit_button('Submit')
     if button:
-        message = message + useremail
+        semail(message)
+        st.info("Your email was delivered successfully!")
